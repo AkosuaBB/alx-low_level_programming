0c6f580988033903da34d643522f27da19cb41fa
AkosuaBB <akosuabbasiedu@gmail.com>
2023-06-15 15:54:27 -0700

    my first c programming task completed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        char c;
+	char c;
 	int i;
-        long l;
-       	long long ll;
+	long l;
+	long long ll;
 	float f;
 
-	printf("size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("size of long int: %lu byte(s)\n", sizeof(along));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("size of a char: %lu byte(s)\n", sizeof(c));
+	printf("size of an int: %lu byte(s)\n", sizeof(i));
+	printf("size of long int: %lu byte(s)\n", sizeof(l));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
-}	
+}
+	
